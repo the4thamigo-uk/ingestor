214db8c2f16b5892cf44f79dcc5b9c1d3082590e
Andrew Smith <andrew.smith@miracl.com>
2018-11-13 20:46:27 +0000

    Make in-memory store the default

--- cmd/reader/main.go
+++ cmd/reader/main.go
@@ -5,25 +5,36 @@ import (
 	log "github.com/sirupsen/logrus"
 	"github.com/spf13/pflag"
 	"github.com/the4thamigo-uk/ingestor/cmd/reader/service"
+	"github.com/the4thamigo-uk/ingestor/pkg/store"
 	"github.com/the4thamigo-uk/ingestor/pkg/store/cassandra"
+	"github.com/the4thamigo-uk/ingestor/pkg/store/memory"
 	"github.com/the4thamigo-uk/interrupter"
 	"os"
 )
 
 func main() {
 	addr := pflag.StringP("address", "c", ":8080", "Address that the server should connect to")
-	sAddr := pflag.StringP("store", "s", "127.0.0.1", "Address of Cassandra host")
+	sAddr := pflag.StringP("store", "s", "", "Address of Cassandra host")
 	pflag.Parse()
 
 	l := log.New()
-	c, err := cassandra.New(*sAddr)
-	if err != nil {
-		handleError(err)
-		return
-	}
-	defer c.Close()
 
-	s := service.New(*addr, c, l)
+	var err error
+	var st store.Store
+
+	if *sAddr == "" {
+		st = memory.New()
+	} else {
+		c, err := cassandra.New("reader", *sAddr)
+		if err != nil {
+			handleError(err)
+			return
+		}
+		defer c.Close()
+		st = c
+	}
+
+	s := service.New(*addr, st, l)
 
 	irpt := interrupter.New(s.Stop)
 	defer irpt.Close()
